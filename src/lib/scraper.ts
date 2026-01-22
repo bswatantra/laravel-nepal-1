18febfd7e6d83581bf93ab30e5f58788bf64cfed
swatantra <swatantra0723@gmail.com>
2026-01-22 12:53:30 +0545

    add chromium tar file

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,10 +1,12 @@
 import puppeteer from "puppeteer";
-import chromium from "@sparticuz/chromium-min";
+import chromium from "@sparticuz/chromium";
 
 export async function getBrowser() {
   return await puppeteer.launch({
     args: chromium.args,
-    executablePath: await chromium.executablePath(),
+    executablePath: await chromium.executablePath(
+      "https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.x64.tar",
+    ),
     headless: true,
     defaultViewport: {
       width: 1280,
