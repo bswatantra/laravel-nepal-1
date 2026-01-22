039455a88eb277525ae313de2ab838c5818e0c6f
swatantra <swatantra0723@gmail.com>
2026-01-22 13:09:46 +0545

    updates

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,11 +1,15 @@
 import puppeteer from "puppeteer";
 import chromium from "@sparticuz/chromium";
+import packageJson from "../../package.json";
 
+const chromiumVersion =
+  packageJson.dependencies["@sparticuz/chromium"]?.replace(/^[\^~]/, "") ||
+  "143.0.4";
 export async function getBrowser() {
   return await puppeteer.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath(
-      "https://github.com/Sparticuz/chromium/releases/download/v143.0.4/chromium-v143.0.4-pack.x64.tar",
+      `https://github.com/Sparticuz/chromium/releases/download/v${chromiumVersion}/chromium-v${chromiumVersion}-pack.x64.tar`,
     ),
     headless: true,
     defaultViewport: {
